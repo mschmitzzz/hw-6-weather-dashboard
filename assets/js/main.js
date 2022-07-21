f788954f5b7739508506e96d6982ebccfb3750a0
Mac Schmitz <mac.schmitz@att.net>
2022-07-21 15:27:58 -0400

    completed fetch api calls

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,38 @@ var weatherDayTempEl = document.querySelector("#weather-day-temp")
 var weatherDayWindEl = document.querySelector("#weather-day-wind")
 var weatherDayHumidityEl = document.querySelector("#weather-day-humidity")
 var weatherDayUvIndexEl = document.querySelector("#weather-day-uv-index")
+var forecastContainerEl = document.querySelector("#forecast-container")
+var weatherDayIconEl = document.querySelector("#weather-day-icon")
 
 var baseUrl = "http://api.openweathermap.org/";
 var apiKey = "902ed934f0d4c4e644f4024796540181";
+
+function populate5Day(data) {
+    data.forEach(function(day, index) {
+        if(index === 0 || index > 5) {
+            return;
+        }
+        var temp = day.temp.day;
+        var windSpeed = day.wind_speed;
+        var humidity = day.humidity.day;
+        var icon = day.weather[0].icon;
+        var div = document.createElement('div');
+        div.classList = 'card-weather col-md-2 col-sm-12 bg-dark text-light me-3'
+        div.innerHTML = `
+        <h4>3/31/2021</h4>
+        <img src="http://openweathermap.org/img/wn/${icon}.png" />
+        <dl>
+          <dt>Temp:</dt>
+          <dd>${temp}</dd>
+          <dt>Wind:</dt>
+          <dd>${windSpeed} MPH</dd>
+          <dt>Humidity:</dt>
+          <dd>${humidity}%</dd>
+        </dl>
+        `
+        forecastContainerEl.appendChild(div);
+    })
+}
 
 function getCityDayWeather(city) {
     var url = `${baseUrl}geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
@@ -20,13 +49,27 @@ function getCityDayWeather(city) {
         var lat = cityObject.lat;
         var lon = cityObject.lon;
 
-        var currentWeatherUrl = `${baseUrl}data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        var currentWeatherUrl = `${baseUrl}data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
 
         fetch(currentWeatherUrl).then(function(response) {
             return response.json()
         })
         .then(function(data) {
-            console.log(data)
+            console.log(data);
+            var current = data.current;
+            var temp = current.temp;
+            var windSpeed = current.wind_speed;
+            var humidity = current.humidity;
+            var uvIndex = current.uvi;
+            var icon = current.weather[0].icon;
+            weatherDayCityEl.textContent = city;
+            weatherDayTempEl.textContent = temp;
+            weatherDayWindEl.textContent = windSpeed;
+            weatherDayHumidityEl.textContent = humidity;
+            weatherDayUvIndexEl.textContent = uvIndex;
+            weatherDayIconEl.src = `http://openweathermap.org/img/wn/${icon}.png`;
+
+            populate5Day(data.daily);
         })
     })
 }
